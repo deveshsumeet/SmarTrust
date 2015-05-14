8525558d1ca01572f5eae196b54808bc6462f384
ajay <ajaykumar_sharma@apple.com>
2015-05-13 18:48:06 -0700

    Adding support for images.

--- workout-tracker/keys/MerchantAddressKeys.js
+++ workout-tracker/keys/MerchantAddressKeys.js
@@ -1,10 +1,9 @@
-
 function define(name, value) {
     Object.defineProperty(exports, name, {
-        value:      value,
+        value: value,
         enumerable: true
     });
 }
 
-define("merchant1", 'mvpM2EnBDd9UspvJH1sYRurvqtbPCHKcHt');		
+define("merchant1", 'mvpM2EnBDd9UspvJH1sYRurvqtbPCHKcHt');
 define("merchant2", 'myfPXggKXyq8DvjCfuaGXNFsDxZbhrEkcx');
