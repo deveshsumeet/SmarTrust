7bbfb9f866045153cddddfe8cafb9cc0b42ecdfe
deveshsumeet <deveshsumeet@gmail.com>
2015-05-14 18:33:04 -0700

    changes in JS

--- workout-tracker/tiecon/custom.js
+++ workout-tracker/tiecon/custom.js
@@ -5,3 +5,22 @@ $(function() {
       return false;
   });
 });
+
+$(document).ready(function(){
+$.ajax({
+    url: "/transactions?restaurantId=smartrust.01@gmail.com",
+    type: "GET",
+    dataType : "json",
+    success: function( data ) {
+		$.each(data, function() {
+  			$.each(this, function(key,value) {
+  				if ( key == "review") {
+  					var $commentBox = $("<article class='comment'><div class='comment-body'><div class='text'><p></p><span class='star'></span></div></div></article>");
+  					var $abc = $commentBox.find("p").text(value);
+                    $('section').append($commentBox);
+  				}	
+  			});
+		});
+    }
+});
+});
